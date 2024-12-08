6add9bd99bb5ea6b5732dc3e3f520d3306cec31c
mlopIT <michaellopezuni@gmail.com>
2024-12-08 16:13:09 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,7 +10,7 @@ def get_base64_image(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode()
 
-image_path = "assets/images/puppy place logo.jpg"
+image_path = "assets/images/pawsforlove.jpg"
 base64_image = get_base64_image(image_path)
 
 # Embed in HTML
